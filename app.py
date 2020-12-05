9bb3103a0cdda2f628f4be1228dc87caa656a313
syedamir9392 <amirsyed8893@gmail.com>
2020-12-06 01:25:15 +0530

    Graph plotter (dummy data) using CSV added

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+# plotting graph here
+import pandas as pd
+
+test = pd.read_csv('test.csv', names=['A', 'B', 'C', 'D'])
+fig = test.plot.line().get_figure()
+fig.savefig('static/images/plot.png')
+
+# app here
 from flask import Flask, render_template
 app = Flask(__name__)
 
